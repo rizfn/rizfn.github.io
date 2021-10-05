c8e36b1359ce13bc23c98eac50bc348348c4947d
Riz <87264061+rizfn@users.noreply.github.com>
2021-10-05 19:27:14 +0530

    added basic parallax scrolling

--- test2.js
+++ test2.js
@@ -16,21 +16,52 @@ function toggleState() { // on clicking the play button in olivier's pic
     aud.pause();
     pic.src="img/ollie_static.png";
   }
+
 };
 
 function scroll() { // used for scrollmagic
+
   var controller = new ScrollMagic.Controller();
 
-  var height = document.getElementById("crossbellbg").offsetHeight + document.getElementById("liberlbg").offsetHeight + document.getElementById("scrollprompt").offsetHeight
+  var liberlheight = document.getElementById("liberlbg").offsetHeight
+  var crossbellheight = document.getElementById("crossbellbg").offsetHeight
+  var pinheight = document.getElementById("scrollprompt").offsetHeight + liberlheight + crossbellheight
 
-  var scene = new ScrollMagic.Scene({
+  // pinning scene
+  new ScrollMagic.Scene({
     triggerElement: "#headerelement", // where to start
-    duration: height, // how long for the scene to last
+    duration: pinheight, // how long for the scene to last
     triggerHook: 0  // when trigger passes the top of the screen
   })
   .setPin("#pinmusicelement")  // pin this
-  .addIndicators({name: "controller"}) // add indicators (requires plugin)
+  .addIndicators({name: "ollie pin"}) // add indicators (requires plugin)
   .addTo(controller);
+
+  // defining parallax animations
+  parallaxliberl = gsap.to("#liberlbg", {y: 200, ease: Linear.easeNone});
+  parallaxcrossbell = gsap.to("#crossbellbg", {y: 200, ease: Linear.easeNone});
+
+  // parallax scene: liberl
+  new ScrollMagic.Scene({
+    triggerElement: "#liberlbg",
+    duration: liberlheight,
+    triggerHook: 0
+  })
+  .setTween(parallaxliberl)
+  .addIndicators({name: "liberl parallax"})
+  .addTo(controller);
+
+  // parallax scene: crossbell
+  new ScrollMagic.Scene({
+    triggerElement: "#crossbellbg",
+    duration: crossbellheight,
+    triggerHook: 0
+  })
+  .setTween(parallaxcrossbell)
+  .addIndicators({name: "crossbell parallax"})
+  .addTo(controller);
+  
+
 };
 
 
