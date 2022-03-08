cc19c39c7aef3195107b50c935f2cdefeccccd71
Riz <87264061+rizfn@users.noreply.github.com>
2022-03-08 13:01:11 +0530

    Testing no-cors (probably won't work)
    acCESS TO fetCH at 'HTTPS://TrAILSiNtHEdaTaBASE.coM/apI/CHr/dEtAIL/staT' fROM ORiGIn 'htTps://rIZfN.gItHUB.io' HaS beEN blOCkEd by COrS POLiCY: NO 'ACCess-CONTROl-AlLoW-ORIgIn' hEadEr Is PREsenT on tHE rEQUESTeD RESOurCE. If AN opaqUE respOnSE SErves YOuR NEeDS, sEt tHe rEquesT's mode TO 'NO-Cors' tO fEtcH thE ResOURcE wITh cORs disABlEd.

--- js/page6.js
+++ js/page6.js
@@ -1,3 +1,3 @@
-d3.json("https://trailsinthedatabase.com/api/chr/detail/stat").then(function (data) {
+d3.json("https://trailsinthedatabase.com/api/chr/detail/stat", mode="no-cors").then(function (data) {
     console.log(data)
 })
