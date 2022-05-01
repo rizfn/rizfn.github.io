cffbd31b005a7a1a01cc75402a02ff583fa6dab0
Riz <87264061+rizfn@users.noreply.github.com>
2022-05-01 18:57:26 +0530

    Built the basics of the force directed graph
    Added in a force directed graph, using d3v7. It uses data from the top 25 sky characters (in terms of rows). Still work to do

--- js/page6.js
+++ js/page6.js
@@ -1,3 +1,83 @@
-d3.json("https://trailsinthedatabase.com/api/chr/detail/stat", mode="no-cors").then(function (data) {
-    console.log(data)
-})
+var margin = {top:50, left:60, bottom:30, right:30},
+    width = 1000 - margin.left - margin.right,
+    height = 620 - margin.left - margin.right;
+
+
+var svg = d3.select("div#forceDirectedGraph")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody().strength(-150))
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+d3.json("data/skytest.json").then(function(graph) {
+
+  console.log(graph);
+
+  var link = svg.append("g")
+      .attr("class", "edges")
+    .selectAll("line")
+    .data(graph.edges)
+    .enter().append("line")
+      .attr("stroke-width", function(d) { return (d.count**0.5)*0.1; })
+      .attr("stroke", "#CC8888");
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+      .attr("r", function(d) {return (d.rows*0.2)**0.5;})
+      .attr("fill", "#CCCCCC")
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+  node.append("title")
+      .text(function(d) { return d.id; });
+
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(graph.edges);
+
+  simulation.force("link")
+      .strength(function(d) {console.log(d); return (d.count**0.5)*0.001;});
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+});
+
+function dragstarted(event, d) {
+  if (!event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(event, d) {
+  d.fx = event.x;
+  d.fy = event.y;
+}
+
+function dragended(event, d) {
+  if (!event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
